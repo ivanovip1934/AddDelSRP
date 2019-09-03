a507632ca25c5247621c727aba1bd18aed185803
ivanovip1934 <ivanovip1934@outlook.com>
2019-09-03 13:53:48 +0300

    Application in work

--- AddDelSRP/Program.cs
+++ AddDelSRP/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Security.Principal;
 using System.Diagnostics;
 using Microsoft.Win32;
+using System.IO;
 //using System.ComponentModel;
 
 namespace AddDelSRP
@@ -14,117 +15,95 @@ namespace AddDelSRP
     {
         static void Main(string[] args) {
 
+            string PathCheckFileName = Path.Combine( Environment.GetEnvironmentVariable("windir"),"AddDelSRP");
             
-            //if (!IsAdministrator()) {
-            //    string PathToApp = System.Reflection.Assembly.GetEntryAssembly().Location;
-            //    var startinfo = new ProcessStartInfo(PathToApp) { Verb = "runas" };
-            //    try {
-            //        Process.Start(startinfo);
-            //        Environment.Exit(0);
-            //    }
-            //    catch (Exception ex) {
-            //        Console.WriteLine("Could not start process.");
-            //        Console.WriteLine(ex);
-            //    }
-                
-
-            //} else  {
-            //    Console.WriteLine("YA ADMIN");
-            //}
-
             List<RegValue> regvalue = new List<RegValue> {
                 new RegValue("{209b1b45-6c84-42bb-861d-d2a187cea34c}","Full access to disk C:", "C:\\","130888527003079690","0"),
                 new RegValue("{c2c2cf69-41c0-496e-aafb-06604377a666}","Full access to disk D:", "D:\\","130888527003079690","0")
             };
 
-            foreach (RegValue _regvalue in regvalue) {
-               // Console.WriteLine(_regvalue.Description);
-            }
+           
             string reg_path = @"SOFTWARE\Policies\Microsoft\Windows\Safer\CodeIdentifiers\262144\Paths";
-            //string reg_path6432 = @"SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\safer\codeidentifiers\262144\Paths";
-            AddKeys(reg_path, regvalue);
-            //AddKeys(reg_path6432, regvalue);
 
-            //DelKeys(reg_path, regvalue); 
-            //DelKeys(reg_path6432, regvalue);
+            if (File.Exists(PathCheckFileName)) {
+                DelKeys(reg_path, regvalue, PathCheckFileName);
+            } else {
+                AddKeys(reg_path, regvalue, PathCheckFileName);
+            }
 
 
-            Cmd("gpupdate /force");
-
-                //Console.WriteLine(System.Reflection.Assembly.GetEntryAssembly().Location);
-                Console.ReadKey();
+            Console.WriteLine("Press any key....");   
+            Console.ReadKey();
         }
 
 
 
 
-        public static void AddKeys(string path, List<RegValue> regvalue) {
-            // int appcount = 0;
-            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(path, true)) {
-                Console.WriteLine();
-                Console.WriteLine("************************************************************");
-                //Console.WriteLine("Key Path: " + key.Name);
-                Console.WriteLine("************************************************************");
-                Console.WriteLine();
-
+        public static void AddKeys(string regpath, List<RegValue> regvalue, string filecheck) {
+            
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(regpath, true)) {
+                int flag = 0;
 
                 foreach (RegValue _regvalue in regvalue) {
-                    //Console.WriteLine(_regvalue);
                     try {
                         using (RegistryKey subkey = key.CreateSubKey(_regvalue.Path)) {
                             subkey.SetValue("Description", _regvalue.Description);
                             subkey.SetValue("ItemData", _regvalue.ItemData);
                             subkey.SetValue("LastModified", _regvalue.LastModified, RegistryValueKind.QWord);
                             subkey.SetValue("SaferFlags", _regvalue.SaferFlags, RegistryValueKind.QWord);
+                            Console.WriteLine("-------------------------------------------");
+                            Console.WriteLine($"        Adding {_regvalue.Description}");
+                            Console.WriteLine("-------------------------------------------");
+                            flag++;
                         }
                     }
                     catch (Exception ex) {
+                        Console.WriteLine($"Error while Adding {_regvalue.Description}");
                         Console.WriteLine(ex.Message);
                     }
+                }
 
+                if (flag == 2) {
+                    File.Create(filecheck);
+                    Cmd();
                 }
                 
             }
         }
 
 
-        public static void DelKeys(string path, List<RegValue> regvalue) {
-            // int appcount = 0;
-            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(path, true)) {
-                Console.WriteLine();
-                Console.WriteLine("************************************************************");
-                //Console.WriteLine("Key Path: " + key.Name);
-                Console.WriteLine("************************************************************");
-                Console.WriteLine();
-
-
+        public static void DelKeys(string regpath, List<RegValue> regvalue, string filecheck) {
+            
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(regpath, true)) {
+                int flag = 0;                
                 foreach (RegValue _regvalue in regvalue) {
-                    //Console.WriteLine(_regvalue);
+                    
                     try {
                         key.DeleteSubKey(_regvalue.Path);
+                        Console.WriteLine("-------------------------------------------");
+                        Console.WriteLine($"        Deleting {_regvalue.Description}");
+                        Console.WriteLine("-------------------------------------------");
+                        flag++;
                     }
                     catch (Exception ex) {
+                        Console.WriteLine($"Error while deleting {_regvalue.Description}");
                         Console.WriteLine(ex.Message);
                     }
+                }
+
+                if (flag == 2) {
+
+                    File.Delete(filecheck);
+                    Cmd();
                 }
 
             }
         }
 
-        public static void Cmd(string line) {
-            Process.Start(new ProcessStartInfo { FileName = "cmd", Arguments = $"/c {line}" }).WaitForExit();
+        public static void Cmd() {
+            Process.Start(new ProcessStartInfo { FileName = "gpupdate.exe", Arguments = $" /target:computer /force"}).WaitForExit();
         }
 
-
-
-
-
-        public static bool IsAdministrator() {
-            using (WindowsIdentity identity = WindowsIdentity.GetCurrent()) {
-                WindowsPrincipal principal = new WindowsPrincipal(identity);
-                return principal.IsInRole(WindowsBuiltInRole.Administrator);                    
-            }
-        }
 
     }
 }
--- AddDelSRP/Properties/AssemblyInfo.cs
+++ AddDelSRP/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AddDelSRP")]
+[assembly: AssemblyTitle("Add or Dell Full Access to disk C and D")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
